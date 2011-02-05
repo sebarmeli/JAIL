5b6b0406fabfb100feb5b8f9cf00dc8921bb878f
Sebastiano Armeli-Battana <seba.armeli@gmail.com>
2011-02-05 12:59:01 +1100

    fix "FadeIn" method not working

--- JqueryAsynchImageLoader.js
+++ JqueryAsynchImageLoader.js
@@ -16,7 +16,7 @@
 	permit persons to whom the Software is furnished to do so, subject to
 	the following conditions:
 
-	The above copyright notice and this permission notice shall be
+	The above copyright notice and this permission notice shall be 
 	included in all copies or substantial portions of the Software.
 
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
@@ -256,13 +256,21 @@
 		// Main function --> Load the images copying the "data-href" attribute into the "src" attribute
 		_loadImage : function(options, $img) {
 
+			$img.hide();
 			$img.attr("src", $img.attr("data-href"));
 			$img.removeAttr('data-href');
 
 			// Images loaded with some effect if existing
 			if(options.effect) {
-				$img[options.effect](options.speed);
+				if (options.speed) {
+					$img[options.effect](parseInt(options.speed, 10));
+				} else {
+					$img[options.effect]();
+				}
+			} else {
+				$img.show();
 			}
+			
 		}
 	};
 }(jQuery));
