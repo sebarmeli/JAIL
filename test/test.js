4374311aaa386b280a7d9a40cae79db5a0365b34
Sebastiano Armeli-Battana <seba.armeli@gmail.com>
2011-12-30 00:06:02 +1100

    Test cases changed according to the new version

--- test/test.js
+++ test/test.js
@@ -6,8 +6,8 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 			name: "TestCase Images loaded after timeout",
 			setUp : function() {
 				$('#container')
-					.append('<img class="lazy" data-href="../sample1.jpg" src="../img/blank.jpg" width="200" height="200"/> ')
-					.append('<img class="lazy" data-href="../sample2.jpg" src="../img/blank.jpg" width="200" height="200" style="position:absolute; left:-1000px;"/> ')
+					.append('<img class="lazy" data-src="../demo/img/sample1.jpg" src="../demo/img/blank.gif" width="200" height="200"/> ')
+					.append('<img class="lazy" data-src="../demo/img/sample2.jpg" src="../demo/img/blank.gif" width="200" height="200" style="position:absolute; left:-1000px;"/> ')
 			},
 			tearDown : function() {
 				$('.lazy').remove();
@@ -18,40 +18,29 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 				$('img.lazy').jail();
 
 				this.wait(function(){
-					Y.Assert.areEqual("../sample1.jpg", $('img.lazy').attr("src"));
-					Y.Assert.isUndefined( $('img.lazy').attr('data-href'));
+					Y.Assert.areEqual("../demo/img/sample1.jpg", $('img.lazy').attr("src"));
+					Y.Assert.isUndefined( $('img.lazy').attr('data-src'));
 				}, 10);
 			},
 
 			"test Images Loaded Asynchronously with 1 seconds Delay - Demo/Example 5" : function () {
 				// Call to the method with timeout
-				$('img.lazy').asynchImageLoader({timeout : 1000 });
+				$('img.lazy').jail({timeout : 1000 });
 
-				Y.Assert.areEqual("../img/blank.jpg", $('img.lazy').attr("src"));
+				Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').attr("src"));
 
 				this.wait(function(){
-					Y.Assert.areEqual("../sample1.jpg", $('img.lazy').attr("src"));
+					Y.Assert.areEqual("../demo/img/sample1.jpg", $('img.lazy').attr("src"));
 				}, 1000);
-			},
-
-			"Visible images should load immediately and hidden images on a delay" : function () {
-				$('img.lazy').asynchImageLoader({ event:'load', timeout : 1000 });
-
-				Y.Assert.areEqual( "../sample1.jpg", $('img.lazy:first').attr("src") );
-				Y.Assert.areEqual( "../img/blank.jpg", $('img.lazy:last').attr("src") );
-
-				this.wait(function(){
-					Y.Assert.areEqual( "../sample2.jpg", $('img.lazy:last').attr("src") );
-				}, 1001);
 			},
 			
 			"Visible images should load immediately with offset 100 - Demo/Example 7" : function () {
-				$('img.lazy').asynchImageLoader({timeout : 1000, offset: 100 });
+				$('img.lazy').jail({timeout : 1000, offset: 100 });
 
-				Y.Assert.areEqual("../img/blank.jpg", $('img.lazy').attr("src"));
+				Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').attr("src"));
 
 				this.wait(function(){
-					Y.Assert.areEqual("../sample1.jpg", $('img.lazy').attr("src"));
+					Y.Assert.areEqual("../demo/img/sample1.jpg", $('img.lazy').attr("src"));
 				}, 1000);
 			}
 		});
@@ -61,8 +50,8 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 			name: "TestCase Images loaded after a click",
 			setUp : function() {
 				$('#container')
-					.append('<a class="link">Link</a><img id="img1" class="lazy" data-href="img1.jpg" src="blank.jpg" width="1" height="10"/><img class="lazy" data-href="img2.jpg" src="blank.jpg" /> ')
-					.append('<a class="link2">Link</a><img class="lazy" data-href="img3.jpg" src="blank.jpg" /><img class="lazy" data-href="img4.jpg" src="blank.jpg" 	/>');
+					.append('<a class="link">Link</a><img id="img1" class="lazy" data-src="../demo/img/sample1.jpg" src="../demo/img/blank.gif" width="1" height="10"/><img class="lazy" data-src="../demo/img/sample2.jpg" src="../demo/img/blank.gif" /> ')
+					.append('<a class="link2">Link</a><img class="lazy" data-src="../demo/img/sample3.jpg" src="../demo/img/blank.gif" /><img class="lazy" data-src="../demo/img/sample4.jpg" src="../demo/img/blank.gif" 	/>');
 			},
 			tearDown : function() {
 				$('.lazy').remove();
@@ -71,28 +60,28 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 			},
 
 			"test All Images Loaded after a click on a link - Demo/Example 2" : function () {
-				$('img.lazy').jail({event: "click", selector : "a.link" });
+				$('img.lazy').jail({event: "click", triggerElement : "a.link" });
 
 				// Click on the link
 				Y.one("a.link").simulate("click");
 
 				this.wait(function() {
-					Y.Assert.areEqual("img1.jpg", $('img.lazy').eq(0).attr("src"));
-					Y.Assert.areEqual("img2.jpg", $('img.lazy').eq(1).attr("src"));
-					Y.Assert.areEqual("img3.jpg", $('img.lazy').eq(2).attr("src"));
-					Y.Assert.areEqual("img4.jpg", $('img.lazy').eq(3).attr("src"));
-				}, 100);
+					Y.Assert.areEqual("../demo/img/sample1.jpg", $('img.lazy').eq(0).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample2.jpg", $('img.lazy').eq(1).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample3.jpg", $('img.lazy').eq(2).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample4.jpg", $('img.lazy').eq(3).attr("src"));
+				}, 500);
 			},
 
 			"test Image Fades in after a click on the placeholder for the image  - Demo/Example 3" : function () {
-				$('img.lazy').asynchImageLoader({event: "click", effect : "fadeIn", placeholder: "img/loader" });
+				$('img.lazy').jail({event: "click", effect : "fadeIn", placeholder: "../demo/img/loader.gif" });
 
 				Y.one("#img1").simulate("click");
 
-				Y.Assert.areEqual("img1.jpg", $('img.lazy').eq(0).attr("src"));
-				Y.Assert.areEqual("img/loader", $('img.lazy').eq(1).attr("src"));
-				Y.Assert.areEqual("img/loader", $('img.lazy').eq(2).attr("src"));
-				Y.Assert.areEqual("img/loader", $('img.lazy').eq(3).attr("src"));
+				Y.Assert.areEqual("../demo/img/sample1.jpg", $('img.lazy').eq(0).attr("src"));
+				Y.Assert.areEqual("../demo/img/loader.gif", $('img.lazy').eq(1).attr("src"));
+				Y.Assert.areEqual("../demo/img/loader.gif", $('img.lazy').eq(2).attr("src"));
+				Y.Assert.areEqual("../demo/img/loader.gif", $('img.lazy').eq(3).attr("src"));
 			}
 		});
 
@@ -100,8 +89,8 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 		var testCaseMouseOverAsynch = new Y.Test.Case({
 			name: "TestCase Images loaded after mousing over on a div",
 			setUp : function() {
-				$('#container').append('<div id="tool" class="tool"></div><img id="img1" class="lazy" data-href="img1.jpg" src="blank.jpg" /><img class="lazy" data-href="img2.jpg" src="blank.jpg" /> ');
-				$('body').append('<div id="wrapper"><div class="container2"><a class="link2">Link</a><img class="lazy" data-href="img3.jpg" src="blank.jpg" /><img class="lazy" data-href="img4.jpg" src="blank.jpg" /></div></div>');
+				$('#container').append('<div id="tool" class="tool"></div><img id="img1" class="lazy" data-src="../demo/img/sample1.jpg" src="../demo/img/blank.gif" /><img class="lazy" data-src="../demo/img/sample2.jpg" src="../demo/img/blank.gif" /> ');
+				$('body').append('<div id="wrapper"><div class="container2"><a class="link2">Link</a><img class="lazy" data-src="../demo/img/sample3.jpg" src="../demo/img/blank.gif" /><img class="lazy" data-src="../demo/img/sample4.jpg" src="../demo/img/blank.gif" /></div></div>');
 			},
 			tearDown : function() {
 				$('.tool').remove().empty();
@@ -111,32 +100,32 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 			},
 
 			"test All Images Loaded after a mouse over on a div" : function () {
-				$('img.lazy').jail({event: "mouseover", selector : ".tool" });
+				$('img.lazy').jail({event: "mouseover", triggerElement : ".tool" });
 
 				//Click on the link
 				Y.one("#tool").simulate("mouseover");
 
 				this.wait(function() {
-					Y.Assert.areEqual("img1.jpg", $('img.lazy').eq(0).attr("src"));
-					Y.Assert.areEqual("img2.jpg", $('img.lazy').eq(1).attr("src"));
-					Y.Assert.areEqual("img3.jpg", $('img.lazy').eq(2).attr("src"));
-					Y.Assert.areEqual("img4.jpg", $('img.lazy').eq(3).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample1.jpg", $('img.lazy').eq(0).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample2.jpg", $('img.lazy').eq(1).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample3.jpg", $('img.lazy').eq(2).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample4.jpg", $('img.lazy').eq(3).attr("src"));
 
 					testCaseMouseOverAsynch.tearDown();
-				}, 100);
+				}, 500);
 			},
 
 			"test Image loading after mousing over - Demo/Example 4" : function() {
 				testCaseMouseOverAsynch.setUp();
 
-				$('img.lazy').asynchImageLoader({event: "mouseover"});
+				$('img.lazy').jail({event: "mouseover"});
 
 				Y.one("#img1").simulate("mouseover");
 
-				Y.Assert.areEqual("img1.jpg", $('img.lazy').eq(0).attr("src"));
-				Y.Assert.areEqual("blank.jpg", $('img.lazy').eq(1).attr("src"));
-				Y.Assert.areEqual("blank.jpg", $('img.lazy').eq(2).attr("src"));
-				Y.Assert.areEqual("blank.jpg", $('img.lazy').eq(3).attr("src"));
+				Y.Assert.areEqual("../demo/img/sample1.jpg", $('img.lazy').eq(0).attr("src"));
+				Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').eq(1).attr("src"));
+				Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').eq(2).attr("src"));
+				Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').eq(3).attr("src"));
 
 				testCaseMouseOverAsynch.tearDown();
 			}
@@ -146,8 +135,8 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 		var testCaseMouseOverAsynch = new Y.Test.Case({
 			name: "TestCase Images loaded after mousing over on a div",
 			setUp : function() {
-				$('#container').append('<div id="tool" class="tool"></div><img id="img1" class="lazy" data-href="img1.jpg" src="blank.jpg" /><img class="lazy" data-href="img2.jpg" src="blank.jpg" /> ');
-				$('body').append('<div id="wrapper"><div class="container2"><a class="link2">Link</a><img class="lazy" data-href="img3.jpg" src="blank.jpg" /><img class="lazy" data-href="img4.jpg" src="blank.jpg" /></div></div>');
+				$('#container').append('<div id="tool" class="tool"></div><img id="img1" class="lazy" data-src="../demo/img/sample1.jpg" src="../demo/img/blank.gif" /><img class="lazy" data-src="../demo/img/sample2.jpg" src="../demo/img/blank.gif" /> ');
+				$('body').append('<div id="wrapper"><div class="container2"><a class="link2">Link</a><img class="lazy" data-src="../demo/img/sample3.jpg" src="../demo/img/blank.gif" /><img class="lazy" data-src="../demo/img/sample4.jpg" src="../demo/img/blank.gif" /></div></div>');
 			},
 			tearDown : function() {
 				$('.tool').remove().empty();
@@ -157,16 +146,16 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 			},
 
 			"test All Images Loaded after a mouse over on a div" : function () {
-				$('img.lazy').asynchImageLoader({event: "mouseover", selector : ".tool" });
+				$('img.lazy').jail({event: "mouseover", triggerElement : ".tool" });
 
 				//Click on the link
 				Y.one("#tool").simulate("mouseover");
 
 				this.wait(function() {
-					Y.Assert.areEqual("img1.jpg", $('img.lazy').eq(0).attr("src"));
-					Y.Assert.areEqual("img2.jpg", $('img.lazy').eq(1).attr("src"));
-					Y.Assert.areEqual("img3.jpg", $('img.lazy').eq(2).attr("src"));
-					Y.Assert.areEqual("img4.jpg", $('img.lazy').eq(3).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample1.jpg", $('img.lazy').eq(0).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample2.jpg", $('img.lazy').eq(1).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample3.jpg", $('img.lazy').eq(2).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample4.jpg", $('img.lazy').eq(3).attr("src"));
 
 					testCaseMouseOverAsynch.tearDown();
 				}, 100);
@@ -175,14 +164,14 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 			"test Image loading after mousing over" : function() {
 				testCaseMouseOverAsynch.setUp();
 
-				$('img.lazy').asynchImageLoader({event: "mouseover"});
+				$('img.lazy').jail({event: "mouseover"});
 
 				Y.one("#img1").simulate("mouseover");
 
-				Y.Assert.areEqual("img1.jpg", $('img.lazy').eq(0).attr("src"));
-				Y.Assert.areEqual("blank.jpg", $('img.lazy').eq(1).attr("src"));
-				Y.Assert.areEqual("blank.jpg", $('img.lazy').eq(2).attr("src"));
-				Y.Assert.areEqual("blank.jpg", $('img.lazy').eq(3).attr("src"));
+				Y.Assert.areEqual("../demo/img/sample1.jpg", $('img.lazy').eq(0).attr("src"));
+				Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').eq(1).attr("src"));
+				Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').eq(2).attr("src"));
+				Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').eq(3).attr("src"));
 
 				testCaseMouseOverAsynch.tearDown();
 			}
@@ -192,8 +181,8 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 			name: "TestCase Images for callbacks",
 			
 			setUp : function() {
-				$('#container').append('<div id="tool" class="tool"></div><img id="img1" class="lazy" data-href="img1.jpg" src="blank.jpg" /><img class="lazy" data-href="img2.jpg" src="blank.jpg" /> ');
-				$('body').append('<div id="wrapper"><div class="container2"><a class="link2">Link</a><img class="lazy" data-href="img3.jpg" src="blank.jpg" /><img class="lazy" data-href="img4.jpg" src="blank.jpg" /></div></div>');
+				$('#container').append('<div id="tool" class="tool"></div><img id="img1" class="lazy" data-src="../demo/img/sample1.jpg" src="../demo/img/blank.gif" /><img class="lazy" data-src="img2.jpg" src="../demo/img/blank.gif" /> ');
+				$('body').append('<div id="wrapper"><div class="container2"><a class="link2">Link</a><img class="lazy" data-src="../demo/img/sample3.jpg" src="../demo/img/blank.gif" /><img class="lazy" data-src="../demo/img/sample4.jpg src="../demo/img/blank.gif" /></div></div>');
 			},
 			
 			tearDown : function() {
@@ -207,14 +196,14 @@ YUI().use("node", "console", "test", "node-event-simulate", function (Y) {
 					testCaseMouseOverAsynch.setUp();
 					var global1 = 3;
 
-					$('img.lazy').asynchImageLoader({event: "mouseover", effect: 'fadeIn',speed : '1500',callbackAfterEachImage : (function(){global1=5;}) });
+					$('img.lazy').jail({event: "mouseover", effect: 'fadeIn',speed : '1500',callbackAfterEachImage : (function(){global1=5;}) });
 
 					Y.one("#img1").simulate("mouseover");
 
-					Y.Assert.areEqual("img1.jpg", $('img.lazy').eq(0).attr("src"));
-					Y.Assert.areEqual("blank.jpg", $('img.lazy').eq(1).attr("src"));
-					Y.Assert.areEqual("blank.jpg", $('img.lazy').eq(2).attr("src"));
-					Y.Assert.areEqual("blank.jpg", $('img.lazy').eq(3).attr("src"));
+					Y.Assert.areEqual("../demo/img/sample1.jpg", $('img.lazy').eq(0).attr("src"));
+					Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').eq(1).attr("src"));
+					Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').eq(2).attr("src"));
+					Y.Assert.areEqual("../demo/img/blank.gif", $('img.lazy').eq(3).attr("src"));
 					Y.Assert.areEqual(5, global1);
 
 					testCaseMouseOverAsynch.tearDown();
